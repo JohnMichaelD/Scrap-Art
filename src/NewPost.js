f26a1a9fcc1a258cfbb7e1d659699fcf61559360
John Darrin <john.m.darrin@gmail.com>
2020-08-04 16:27:56 -0700

    new post page coming in

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './NewPost.css';
 
-import headerBackground from './wood.jpg'
-import logo from './scraplogo.png'
-import message from './message.png'
-import newPost from './newpost.png'
-import list from './list.png'
-import footerBackground from './wood2.jpg'
-import glass from './mag\ glass.png'
+import headerBackground from './wood3.jpg'
+import footerBackground from './wood.jpg'
+import addPhotoIcon from './add-image.png'
 
 class NewPost extends React.Component {
   constructor(props) {
@@ -16,11 +12,46 @@ class NewPost extends React.Component {
 
   render() {
     return(
-      <div className="App">
-        <header className="App-header" style={{'background-image': 'url("./wood.jpg")'}}></header>
-          <div className="header" style={{'background-image': 'url("./wood.jpg")'}}>
-            hello
+      <div className="appP">
+        <header className="appPost" style={{'background-image': 'url("./wood3.jpg")'}}>
+          <div className="headerPost">
+            Create New Post
           </div>
+        </header>  
+          <div className="bodyPost">
+            <div className="addPhotos">
+              <img className="addPhotoIcon" src="./add-image.png"></img>
+              Add Photos
+            </div>
+            <div className="postTitle">
+              <input className="postInput" placeholder="Title"></input>
+            </div>
+            <div className="postPrice">
+              <input className="postInput" placeholder="Price"></input>
+            </div>
+            <div className="postCategory">
+              Category <img className="dropDown" src="./dropdownarrow.png"></img>
+            </div>
+            <div className="postInfo">
+                <div className="postLocation">
+                  Location
+                </div> 
+                <div className="postSeller">
+                Name 
+                </div>
+            </div>
+            <div className="postEmail">
+              <input className="postInput" placeholder="Email"></input>
+            </div>
+            <div className="postDescription">
+              <input className="postInputDescription" placeholder="Description"></input>
+            </div>
+          </div>
+        <footer className="footerPost" style={{'background-image': 'url("./wood.jpg")'}}>
+            <div className="postButton">
+            Post
+            </div>
+        </footer>
       </div>
     )
   }
